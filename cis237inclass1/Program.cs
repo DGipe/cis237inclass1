359ff5bb5dcc0f2576847e6ddbd0adb8f95cf1d2
David Gipe <dwgipe@gmail.com>
2017-01-18 19:55:18 -0500

    Add Employee and UserInerface class. Update prigram to use new classes

--- cis237inclass1/Program.cs
+++ cis237inclass1/Program.cs
@@ -10,6 +10,58 @@ namespace cis237inclass1
     {
         static void Main(string[] args)
         {
+            //Make a new instance of the user interface class
+            UserInterface ui = new UserInterface();
+
+
+            //Here is a new employying being created using the default 
+            //constructor that we wrote in the employee class
+            //Even though we arnt using it, I want you to see a single instance
+            //of one vs an array of them.
+            Employee myEmployee = new Employee();
+
+            //Lets make an array to hold a bumch of instances of the Employee class
+            Employee[] employees = new Employee[10];
+
+            //Lets add some employees to our array
+            employees[0] = new Employee("David", "Barnes", 835.00m);  //"m" a the end convers to a decimal
+            employees[1] = new Employee("James", "Kirk", 453.00m);
+            employees[2] = new Employee("Jean-Luc", "Pircard", 290.00m);
+            employees[3] = new Employee("Benjamin", "Sisko", 587.30m);
+            employees[4] = new Employee("John", "Deere", 456.00m);
+            employees[5] = new Employee("Poop", "Nuggett", 684.00m);
+
+            //Get some input from the user
+            int choice = ui.GetUserInput();
+
+            //While the chouse they slected is not 2, continue to do work
+            while (choice != 2)
+            {
+                //See if the input they sent is equal to 1.
+                if (choice == 1)
+                {
+                    //Create a sring that can me concated to 
+                    string outputString = "";
+
+                    //Print out the emploees in the array
+                    foreach (Employee employee in employees)
+                    {
+                        if (employee != null)
+                        {
+
+                            //concat to te outputstring
+                            outputString += employee.ToString() +
+                                Environment.NewLine;
+                        }
+                    }
+
+                    //Use the US class to print out the string
+                    ui.Output(outputString);
+                }
+
+                //re-prompt te user for input
+                choice = ui.GetUserInput();
+            }
         }
     }
 }
---
+++ cis237inclass1/UserInterface.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass1
+{
+    class UserInterface
+    {
+        //No Variables
+        //No Properties
+        //No Construcors (but we could if we needed to)
+
+        //*****************************
+        //Methods
+        //*****************************
+        public int GetUserInput()
+        {
+            //Print out a menu
+            this.PrintMenu();  //calling a method
+
+            //Get the input from the user
+            String input = Console.ReadLine();
+
+            //While te imput is not valid re-get the input
+            while(input != "1" && input != "2")
+                {
+
+                //Print Error message
+                this.PrintErrorMessage();
+
+                //re-pring the menu
+                this.PrintMenu();
+
+                //et the input from the console again
+                input = Console.ReadLine();
+
+                }
+            //At this pint, I kow the input is a valid number of either
+            //a 1 or a 2.
+
+            return Int32.Parse(input);
+        }
+
+        public void Output(string outputString)
+        {
+            Console.WriteLine(outputString);
+        }
+
+
+        private void PrintMenu()
+        {
+            Console.WriteLine("What would you like to do today?");
+            Console.WriteLine("1. Print List");
+            Console.WriteLine("2. Exit");
+        }
+
+        private void PrintErrorMessage()
+        {
+            Console.WriteLine("That is not a valid entry Dave");
+            Console.WriteLine("Please make a valid choice");
+            Console.WriteLine();
+        }
+    }
+}
